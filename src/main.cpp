d3e182a2eaa3ab34e378bea7ef295713be0de2c0
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2018-11-12 13:40:15 +0000

    Three LEDs with better timing

--- src/main.cpp
+++ src/main.cpp
@@ -9,27 +9,36 @@ DigitalOut   red(LED1,1); /* initialise to 1 = off */
 DigitalOut green(LED2,1); /* initialise to 1 = off */
 DigitalOut  blue(LED3,1); /* initialise to 1 = off */
 
+enum { ON, OFF};
 void flash_red(void)
 {
 	while(true) {
-		red = !red;
+		red = ON;
 		wait(0.3);
+		red = OFF;
+		wait(0.6);
 	}
 }
 
 void flash_green(void)
 {
+	wait(0.3);
 	while(true) {
-		green = !green;
+		green = ON;
 		wait(0.3);
+		green = OFF;
+		wait(0.6);
 	}
 }
 
 void flash_blue(void)
 {
+	wait(0.6);
 	while(true) {
-		blue = !blue;
+		blue = ON;
 		wait(0.3);
+		blue = OFF;
+		wait(0.6);
 	}
 }
 
